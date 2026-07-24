bc8193ad134d1308adbe7146664671e3f9c3bb5a
meegaw-roophy <elvisobulemire006@gmail.com>
2026-07-24 14:31:11 +0300

    fix CORS middleware parameter name

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,7 +6,7 @@ app = FastAPI(title="VEKTRA API")
 
 app.add_middleware(
     CORSMiddleware,
-    origins = [
+    allow_origins = [
         "http://localhost:5500",
         "http://127.0.0.1:5500",
         "http://localhost:5501",
