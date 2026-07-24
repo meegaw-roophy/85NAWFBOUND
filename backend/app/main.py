df70dc027fad5f44368dedd6b832ab0e59085832
meegaw-roophy <elvisobulemire006@gmail.com>
2026-07-24 14:13:01 +0300

    fix CORS for production

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,11 +6,21 @@ app = FastAPI(title="VEKTRA API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://127.0.0.1:5505", # Your specific Live Server address
-        "http://localhost:5505",
-        "http://127.0.0.1:5500", # Fallback for default Live Server ports
+    origins = [
         "http://localhost:5500",
+        "http://127.0.0.1:5500",
+        "http://localhost:5501",
+        "http://127.0.0.1:5501",
+        "http://localhost:5502",
+        "http://127.0.0.1:5502",
+        "http://localhost:5503",
+        "http://127.0.0.1:5503",
+        "http://localhost:5504",
+        "http://127.0.0.1:5504",
+        "http://localhost:5505",
+        "http://127.0.0.1:5505",
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
         "https://meegaw-roophy.github.io",
     ],
     allow_credentials=True,
